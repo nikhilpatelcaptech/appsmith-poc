e07c94e473d00d0d71d26a4be0c4897a73f391aa
Nikhil Patel <nikhil.patel@capillarytech.com>
2023-06-13 20:56:28 +0530

    docker file and shell script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM appsmith/appsmith-ee:latest
 ENV BASE_DIR=/opt/capillary/appsmith-poc
+CMD ["/bin/bash","/getAppsmithMongoURI.sh"]
 RUN echo $BASE_DIR
 RUN mkdir -p $BASE_DIR
 WORKDIR $BASE_DIR
---
+++ getAppsmithMongoURI.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export APPSMITH_MONGODB_URI="mongodb://${CAMPAIGNS_DB_MONGO_MASTER_USERNAME}:${CAMPAIGNS_DB_MONGO_MASTER_PASSWORD}@campaigns-db-mongo-master.default:27017/${APPSMITH_DB_NAME}?authSource=admin";
+export APPSMITH_REDIS_URL="redis://${REDIS_HOST}:${REDIS_PORT}"
+
+exit 0
